e4c42f2c58abd9e302e0c25eb265a2b24ac03cbd
zzizi <zzi_zi@naver.com>
2025-05-30 20:47:44 +0900

    fix: android 설정파일 변경

---
+++ android/app/src/main/java/com/kinover_frontend/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.kinover_frontend
+
+import android.os.Bundle
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+// import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+
+class MainActivity : ReactActivity() {
+
+    override fun getMainComponentName(): String = "kinover_frontend"
+
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)  // 🟢 `null` 제거 후 정상 호출
+    }
+}
